9be03d6d4a53ef9e864aaad08c95eaf0192e2e27
David Langley <langley.dave@gmail.com>
2023-06-02 16:19:55 +0100

    add package template before any release

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let releaseVersion = "0.12.0"
+let relaseChecksum = ""
+let url = "https://github.com/matrix-org/dendrite-swift/releases/download/\(releaseVersion)/DendriteP2PGobind.xcframework.zip"
+
+let package = Package(
+    name: "dendrite-swift",
+    products: [
+        .library(
+            name: "DendriteSwift",
+            targets: [
+                "DendriteSwift"
+            ]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "DendriteSwift",
+            url: url,
+            checksum: releaseVersion
+        )
+    ]
+)
