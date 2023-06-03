2b033fdedf3d8b645427b386c9b80df19730dbc4
David Langley <langley.dave@gmail.com>
2023-06-03 19:51:43 +0100

    Updated binary package version to 0.12.0

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let releaseVersion = "0.12.0"
-let releaseChecksum = "empty"
+let releaseChecksum = "2aeb0ee751015d7c903d0be838c9882e0227d1229098f5bdeaa44d14849cfc87"
 let url = "https://github.com/matrix-org/dendrite-swift/releases/download/\(releaseVersion)/DendriteP2PGobind.xcframework.zip"
 
 let package = Package(
